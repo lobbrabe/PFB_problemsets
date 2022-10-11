166bdcdde100d1d1cb63af25db0c8e6196eb1e66
student <student@info-18.cshl.edu>
2022-10-11 15:24:10 -0400

    working on part2, if else statements

---
+++ python_prob_ii.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#truth value statement 
+
+x=99
+if x>70:
+  x>70
+  print('True, Number is greater than 70')
+else:
+  x<70
+  print('Not True, Number is less than 70')
+
+
